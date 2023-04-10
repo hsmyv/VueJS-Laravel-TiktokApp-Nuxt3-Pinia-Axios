cc36bf077fc2f8f87b44fba7862bd69c2e5cab90
Hasan <hsmusayev@gmail.com>
2023-04-10 17:09:10 +0400

    update

--- app.vue
+++ app.vue
@@ -1,7 +1,8 @@
 <template>
     <NuxtPage />
-    <AuthOverlay v-if="isLoginOpen"/>
-    <EditProfileOverlay v-if="isEditProfileOpen"/>
+
+    <AuthOverlay v-if="isLoginOpen" />
+    <EditProfileOverlay v-if="isEditProfileOpen" />
 </template>
 
 <script setup>
@@ -9,4 +10,24 @@ import { storeToRefs } from 'pinia';
 const { $userStore, $generalStore } = useNuxtApp()
 const { isLoginOpen, isEditProfileOpen } = storeToRefs($generalStore)
 
+onMounted(async () => {
+    $generalStore.bodySwitch(false)
+    isLoginOpen.value = false
+    isEditProfileOpen.value = false
+
+    try {
+        await $generalStore.hasSessionExpired()
+        await $generalStore.getRandomUsers('suggested')
+        await $generalStore.getRandomUsers('following')
+
+        if ($userStore.id) {
+            $userStore.getUser()
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+watch(() => isLoginOpen.value, (val) => $generalStore.bodySwitch(val))
+watch(() => isEditProfileOpen.value, (val) => $generalStore.bodySwitch(val))
 </script>
